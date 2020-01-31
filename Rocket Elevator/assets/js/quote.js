42f8fe39e4642168b72458298db7e8145b17a66f
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-01-31 15:37:01 -0500

    Residential card calculates recommendation dynamically

--- Rocket Elevator/assets/js/quote.js
+++ Rocket Elevator/assets/js/quote.js
@@ -1,3 +1,22 @@
+// Calculate recommended elevators for Residential
+
+function recommendResidential(){
+    var floors = $("#resfloors").val();
+    var apartments = $("#resapartments").val();
+    var apartmentsByFloor = Math.ceil(apartments / floors);
+    var elevators = Math.ceil(apartmentsByFloor / 6);
+    var numColumns = Math.ceil(floors / 20);
+    var totalElevators = elevators * numColumns;
+    var isPlural = "s";
+
+    if(totalElevators > 1){
+        document.getElementById("restotal").innerHTML = totalElevators;
+        document.getElementById("resplural").innerHTML = isPlural;
+    }else {
+        document.getElementById("restotal").innerHTML = "1";
+    }
+};
+
 // Display appropriate product card via the select element
 var selector = document.getElementById("project_type");
 var selection;
@@ -8,6 +27,13 @@ selector.addEventListener("change", function(){
     $(".quoter").fadeOut();
     var curProductCard = "#" + selection;
     var $curProductCard = $(curProductCard);
-    setTimeout(() => {  $curProductCard.fadeIn(); }, 600)
+    setTimeout(() => {  $curProductCard.fadeIn(); }, 600);
+    if(selection === "residential"){
+        recommendResidential();
+    }
 })
 
+// Recalculate recommended elevators for residential when steppers change
+$(".resinput").change(function(){
+    recommendResidential();
+});
