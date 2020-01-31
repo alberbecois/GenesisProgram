43ba1c4ca5e010479b0a0d5269a165ca80d660b5
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-01-31 16:54:47 -0500

    Calculators added for hybrid and corporate

--- Rocket Elevator/assets/js/quote.js
+++ Rocket Elevator/assets/js/quote.js
@@ -7,13 +7,48 @@ function recommendResidential(){
     var elevators = Math.ceil(apartmentsByFloor / 6);
     var numColumns = Math.ceil(floors / 20);
     var totalElevators = elevators * numColumns;
-    var isPlural = "s";
 
     if(totalElevators > 1){
         document.getElementById("restotal").innerHTML = totalElevators;
-        document.getElementById("resplural").innerHTML = isPlural;
+        document.getElementById("resplural").innerHTML = "s";
     }else {
         document.getElementById("restotal").innerHTML = "1";
+    }
+};
+
+// Calculate recommended elevators for Corporate
+
+function recommendCorporate(){
+    var floors = $("corporatefloors").val();
+    var basements = $("corporatebasements").val();
+    var columns = Math.ceil((floors + basements) / 20);
+    var totalOccupants = floors * ($("corporateoccupants").val());
+    var elevators = Math.ceil(totalOccupants / 1000);
+    var totalElevators = Math.ceil(elevators / columns) * columns;
+
+    if(totalElevators > 1){
+        document.getElementById("corporatetotal").innerHTML = totalElevators;
+        document.getElementById("corporateplural").innerHTML = "s";
+    }else {
+        document.getElementById("corporatetotal").innerHTML = "1";
+    }
+};
+
+// Calculate recommended elevators for Hybrid
+
+function recommendHybrid(){
+    var floors = $("hybridfloors").val();
+    var basements = $("hybridbasements").val();
+    var columns = Math.ceil((floors + basements) / 20);
+    var totalOccupants = floors * ($("hybridoccupants").val());
+    var elevators = Math.ceil(totalOccupants / 1000);
+    var totalElevators = Math.ceil(elevators / columns) * columns;
+
+    if(totalElevators > 1){
+        document.getElementById("hybridtotal").innerHTML = totalElevators;
+        document.getElementById("hybridplural").innerHTML = "s";
+    }else {
+        document.getElementById("hybridtotal").innerHTML = "1";
     }
 };
 
@@ -30,6 +65,10 @@ selector.addEventListener("change", function(){
     setTimeout(() => {  $curProductCard.fadeIn(); }, 600);
     if(selection === "residential"){
         recommendResidential();
+    }else if(selection === "corporate") {
+        recommendCorporate();
+    }else if (selection === "hybrid") {
+        recommendHybrid();
     }
 })
 
