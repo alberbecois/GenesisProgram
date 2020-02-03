d44f91d3daccde71f9ca07949b177d96c69d6cc7
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-02-03 16:19:22 -0500

    Finished quote

--- Rocket Elevator/assets/js/quote.js
+++ Rocket Elevator/assets/js/quote.js
@@ -1,11 +1,12 @@
 // Calculate recommended elevators for Residential
 function recommendResidential(){
-    var floors = $("#resfloors").val();
-    var apartments = $("#resapartments").val();
+    var floors = parseInt($("#resfloors").val());
+    var apartments = parseInt($("#resapartments").val());
     var apartmentsByFloor = Math.ceil(apartments / floors);
     var elevators = Math.ceil(apartmentsByFloor / 6);
     var numColumns = Math.ceil(floors / 20);
     var totalElevators = elevators * numColumns;
+    quotedElevators = totalElevators;
 
     if(totalElevators > 1){
         document.getElementById("restotal").innerHTML = totalElevators;
@@ -17,12 +18,13 @@ function recommendResidential(){
 
 // Calculate recommended elevators for Corporate or Hybrid
 function recommendElevators(quoteType){
-    var floors = $(`#${quoteType}floors`).val();
-    var basements = $(`#${quoteType}basements`).val();
+    var floors = parseInt($(`#${quoteType}floors`).val());
+    var basements = parseInt($(`#${quoteType}basements`).val());
     var columns = Math.ceil((floors + basements) / 20);
-    var totalOccupants = floors * ($(`#${quoteType}occupants`).val());
+    var totalOccupants = (floors + basements) * parseInt(($(`#${quoteType}occupants`).val()));
     var elevators = Math.ceil(totalOccupants / 1000);
     var totalElevators = Math.ceil(elevators / columns) * columns;
+    quotedElevators = totalElevators;
 
     if(totalElevators > 1){
         document.getElementById(`${quoteType}total`).innerHTML = totalElevators;
@@ -32,14 +34,22 @@ function recommendElevators(quoteType){
     }
 }
 
+// Set elevators for Commercial
+function setElevators(){
+    var elevators = parseInt($("#commercialelevators").val());
+    quotedElevators = elevators;
+}
+
 // Display appropriate product card via the select element
 var selector = document.getElementById("project_type");
 var selection;
+var quotedElevators;
 
 selector.addEventListener("change", function(){
     selection = selector.value;
     $(".quoter").fadeOut();
     isElevatorSelectionShowing = false;
+    isFinalQuoteShowing = false;
     var curProductCard = "#" + selection;
     var $curProductCard = $(curProductCard);
     setTimeout(() => {  $curProductCard.fadeIn(); }, 600);
@@ -47,6 +57,8 @@ selector.addEventListener("change", function(){
         recommendResidential();
     }else if(selection !== "commercial") {
         recommendElevators(selection);
+    }else {
+        setElevators();
     }
 })
 
@@ -60,6 +72,11 @@ $(".recommendinput").change(function(){
     recommendElevators(selection);
 });
 
+// Recalculate pricing for Commercial
+$("#commercialelevators").change(function(){
+    setElevators();
+});
+
 // Next button displays the Elevator Selector card
 
 var $nextButtons = $(".nextbutton");
@@ -71,3 +88,57 @@ $($nextButtons).click(function(){
         isElevatorSelectionShowing = true;
     }
 });
+
+// Final Calculator
+function updateTotal(){
+    var selectedElevator = document.querySelector('input[name="elevatortype"]:checked').value;
+    if(selectedElevator === "standard"){
+        var elevatorPrice = quotedElevators * 7565;
+        var elevatorString = elevatorPrice.toFixed(2);
+        document.getElementById("selectedtype").innerHTML = "Standard";
+        document.getElementById("productprice").innerHTML = elevatorString;
+        var installationPrice = elevatorPrice * 0.1;
+        var installationString = installationPrice.toFixed(2);
+        document.getElementById("installationprice").innerHTML = installationString;
+        var totalPrice = elevatorPrice + installationPrice;
+        var totalString = totalPrice.toFixed(2);
+        document.getElementById("totalprice").innerHTML = totalString;
+    } else if(selectedElevator === "premium"){
+        var elevatorPrice = quotedElevators * 12345;
+        var elevatorString = elevatorPrice.toFixed(2);
+        document.getElementById("selectedtype").innerHTML = "Premium";
+        document.getElementById("productprice").innerHTML = elevatorString;
+        var installationPrice = elevatorPrice * 0.13;
+        var installationString = installationPrice.toFixed(2);
+        document.getElementById("installationprice").innerHTML = installationString;
+        var totalPrice = elevatorPrice + installationPrice;
+        var totalString = totalPrice.toFixed(2);
+        document.getElementById("totalprice").innerHTML = totalString;
+    } else {
+        var elevatorPrice = quotedElevators * 15400;
+        var elevatorString = elevatorPrice.toFixed(2);
+        document.getElementById("selectedtype").innerHTML = "Excelium";
+        document.getElementById("productprice").innerHTML = elevatorPrice;
+        var installationPrice = elevatorPrice * 0.16;
+        var installationString = installationPrice.toFixed(2);
+        document.getElementById("installationprice").innerHTML = installationString;
+        var totalPrice = elevatorPrice + installationPrice;
+        var totalString = totalPrice.toFixed(2);
+        document.getElementById("totalprice").innerHTML = totalString;
+    }
+};
+
+// Displays Final Quote
+
+var isFinalQuoteShowing = false;
+
+$("#calculate").click(function(){
+    if(isFinalQuoteShowing === false){
+        updateTotal();
+        $("#finalquote").fadeIn();
+        isFinalQuoteShowing = true;
+    }
+    else{
+        updateTotal();
+    }
+})
